c657e1dc2bf46f0e94ebdccc9cbbd2c2653391d5
Hackers Colony Official <103638018+Hackerscolonyofficial@users.noreply.github.com>
2022-06-22 23:26:17 -0700

    Add files via upload

---
+++ bestX.sh
@@ -0,0 +1,115 @@
+# hi ! my name is azhar and i am a owner of hacker colony officail
+# this tool made by    
+# usr/bin/bash
+echo "Installing Requirements"
+sleep 2
+sudo apt install boxes
+echo " it work on only kali linux ! don't be use in termux  "
+clear  
+echo '        @@@@@@@@@@@@@@@@@@  ' 
+echo '      @@@@@@@@@@@@@@@@@@@@@@@  ' 
+echo '    @@@@@@@@@@@@@@@@@@@@@@@@@@@  ' 
+echo '   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ' 
+echo '   @@@@@@@@@@@@@@@/      \@@@/   @  ' 
+echo '  @@@@@@@@@@@@@@@@\      @@  @___@  ' 
+echo '  @@@@@@@@@@@@@ @@@@@@@@@@  | \@@@@@  ' 
+echo '  @@@@@@@@@@@@@ @@@@@@@@@\__@_/@@@@@  ' 
+echo '   @@@@@@@@@@@@@@@/,/,/./,/_|.\,\,\  ' 
+echo '     @@@@@@@@@@@@@/     HACKER     \  ' 
+echo '                   \_______________/  welcome sir lets hack the whole world  '  
+echo '    __________________________________________________________________________  '   
+sleep 1 
+echo "whats ur name"
+read name 
+echo "hi! $name" 
+sleep 2 
+echo " rule! type number not name ! "
+sleep 2
+echo "⚙"
+sleep 1
+echo "⚙"
+sleep 1 
+echo "⚙"
+sleep 2
+clear 
+boxes main.txt
+sleep 1 
+echo -n " input the number : "
+read number 
+if [ "$number" = "1" ];
+then 
+  git clone https://github.com/alpkeskin/mosint 
+
+fi
+ 
+if [ "$number" = "2" ];
+then 
+  git clone https://github.com/Morsmalleo/HiddenEye
+
+fi 
+
+if [ "$number" = "3" ];
+then 
+  git clone  https://github.com/nccgroup/autopwn 
+
+fi 
+
+if [ "$number" = "4" ];
+then 
+  git clone https://github.com/FluxionNetwork/fluxion
+
+fi
+
+if [ "$number" = "5"  ]; 
+then 
+  git clone https://github.com/Datalux/Osintgram
+
+fi
+
+if [ "$number" = "6" ];
+then 
+  git clone https://github.com/jofpin/trape
+
+fi 
+
+if [ "$number" = "7" ];
+then 
+  git clone https://github.com/sundowndev/phoneinfoga 
+
+fi 
+
+if [ "$number" = "8" ];
+then 
+  sudo apt install Sublist3er
+
+fi  
+
+if [ "$number" = "9" ];
+then 
+  sudo apt install hydra 
+
+fi  
+
+if [ "$number" = "10" ];
+then 
+  git clone https://github.com/EntySec/Ghost
+
+fi  
+
+sleep 2
+echo "________________________________________________________________________________________________________________________________________________________________"
+read -p "||>>>>>>>>>>>>>>||Do you want to see our instagram pages||<<<<<<<<<<<<<|| (y/n)"  yn  
+
+case $yn in 
+        y ) open page.html;;
+        n ) echo "Thanks to u for using this tool!!!!" | boxes -d ian_jones; 
+                   exit;;          
+        * ) echo "Thanks to u for using this tool!!!!" | boxes -d ian_jones;
+                   exit 1 ;; 
+esac
+
+echo "Thanks to u for using this tool!!!!" | boxes -d ian_jones
+
+
+
+
